fd11672b5a028c0142198e199ebb39c990bd41dd
tarunon <croissant9603@gmail.com>
2017-03-30 20:31:24 +0900

    Initial Commit

---
+++ Crash/Crash.h
@@ -0,0 +1,19 @@
+//
+//  Crash.h
+//  Crash
+//
+//  Created by ST90872 on 2017/03/30.
+//  Copyright © 2017年 tarunon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Crash.
+FOUNDATION_EXPORT double CrashVersionNumber;
+
+//! Project version string for Crash.
+FOUNDATION_EXPORT const unsigned char CrashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Crash/PublicHeader.h>
+
+
